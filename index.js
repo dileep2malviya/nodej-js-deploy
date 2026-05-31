530468ba6387e4fdf5ef6458bac529e0631a291b
kk <dileep2malviya@gmail.com>
2026-05-31 16:22:11 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+const PORT = process.env.port ?? 8080
+
+app.get('/',(req, res) => {
+    return res.json({msg: "hello from the server"})
+})
+
+app.listen(PORT, ()=> {
+    console.log('running application on 8080 port')
+})
